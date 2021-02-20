5c999658b742a6f93ef21e0d86e785a3f199e9e9
song12-ran <spabinash2626@gmail.com>
2021-02-20 08:23:28 +0545

    second commit

--- src/chapter_One/Exercise1_4.java
+++ src/chapter_One/Exercise1_4.java
@@ -8,5 +8,12 @@ a a^2 a^3
 package chapter_One;
 
 public class Exercise1_4 {
+    public static void main(String[] args) {
+        System.out.println("a   a^2    a^3");
+        System.out.println("1   1       1");
+        System.out.println("2   4       8");
+        System.out.println("3   9       27");
+        System.out.println("4   16      64");
+    }
 
 }
