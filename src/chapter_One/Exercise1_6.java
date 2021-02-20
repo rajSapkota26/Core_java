83c024f80b9ac5ac2e0e75ae12eb69ec234157a5
song12-ran <spabinash2626@gmail.com>
2021-02-20 08:29:58 +0545

    third commit

--- src/chapter_One/Exercise1_6.java
+++ src/chapter_One/Exercise1_6.java
@@ -3,4 +3,7 @@
 package chapter_One;
 
 public class Exercise1_6 {
+    public static void main(String[] args) {
+        System.out.println(1+2+3+4+5+6+7+8+9);
+    }
 }
--- src/chapter_One/Exercise1_7.java
+++ src/chapter_One/Exercise1_7.java
@@ -2,4 +2,8 @@
 package chapter_One;
 
 public class Exercise1_7 {
+    public static void main(String[] args) {
+        System.out.println(4*(1-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)));
+        System.out.println(4*(1-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)+(1/13)));
+    }
 }
